3b4dc88f3a020eaa34887c45b5ed47bb1412ce17
czeller <mascot27@users.noreply.github.com>
2017-10-09 03:13:40 -0700

    simple I said

--- server.go
+++ server.go
@@ -6,12 +6,22 @@ import (
 	"net/http"
 	"github.com/gorilla/websocket"
 	"fmt"
+	//"WebservicesLogic"
 )
 
 // our host address's flag
 var addr = flag.String("addr", "0.0.0.0:8080", "http service address")
 
 var upgrader = websocket.Upgrader{} // use default options
+
+
+type MessageFormatFromStandardClient struct {
+	LoginMethod string
+	UserName string
+	Password string
+	FunctionName string
+	arguments []string
+}
 
 
 // our echo function
@@ -33,6 +43,11 @@ func echo(w http.ResponseWriter, r *http.Request) {
 
 		// received a message
 		log.Printf("received: %s", message)
+		isValidFormat := validateRequestValidFormat(message)
+		if isValidFormat {
+			fmt.Println("valid format")
+		}
+
 		// send the message to the client
 		err = conn.WriteMessage(mt, message)
 
@@ -44,39 +59,56 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func getUserRole(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
+
+func validateRequestValidFormat(message []byte)(bool){
+	// check length not nil
+	// message get part
+
+	/* message composition:
+
+	[loginMethod][UserName|Password][functionName][argumentsArray]
+
+	arguments array is a string array
+	this implie that we need to check format when sending
+	thus, a username/functionName/password/argument cannot contain a '[', a ']' or a '|'
+
+ 	var my_message MessageFormatFromStandardClient
+	 */
+
+	// validate format
+	opening_counter := 0
+	closing_counter := 0
+
+	for i:= 0; i < len(message) ; i++  {
+		// count '[' and ']' --> this need to be the same number to be valid
+		if message[i] == '[' {
+			opening_counter++
+		} else if message[i] == ']'{
+			closing_counter++
+		}
 	}
-	defer conn.Close()
 
-	for {
-		asker, message, err := conn.ReadMessage()
-		if err  != nil {
-			log.Println("read error: ", err)
-			break
-		}
-
-		if message != nil {
-			fmt.Printf("user to get role: %s\n", message)
-		}
-
-		err = conn.WriteMessage(asker, message)
-		if err != nil {
-			break
-		}
+	if closing_counter != opening_counter {
+		return false
 	}
 
+	if opening_counter != 4 {
+		return false
+	}
+
+	return true
 }
 
+
 func main() {
+	fmt.Println("server started....")
 	flag.Parse()  // ->
 	log.SetFlags(0)
 
+	//lematin := WebservicesLogic.SayHello2()
+
+	//fmt.Print(lematin)
 	http.HandleFunc("/echo", echo)
-	http.HandleFunc("/getUserRole", getUserRole)
 
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
